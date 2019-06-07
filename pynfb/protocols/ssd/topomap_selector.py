57af2784956ac7f53dca03958907dd247a50488a
dmalt <dm.altukhov@ya.ru>
2019-06-07 14:50:37 +0300

    fix filtering in ICA dialog

--- pynfb/protocols/ssd/topomap_selector.py
+++ pynfb/protocols/ssd/topomap_selector.py
@@ -1,13 +1,11 @@
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtWidgets
 from ...protocols.ssd.ssd import ssd_analysis
 from ...protocols.ssd.sliders import Sliders
 from ...protocols.ssd.topomap_canvas import TopographicMapCanvas
 from ...protocols.ssd.interactive_barplot import ClickableBarplot
 
-from ...widgets.parameter_slider import ParameterSlider
-from numpy import arange, dot, array, eye
+from numpy import arange, dot
 from numpy.linalg import pinv
-
 
 
 class TopomapSelector(QtWidgets.QWidget):
@@ -46,11 +44,14 @@ class TopomapSelector(QtWidgets.QWidget):
         self.component_spinbox.setRange(1, len(names))
         self.component_spinbox.valueChanged.connect(self.change_topomap)
         self.component_spinbox.valueChanged.connect(self.draw_lambda_level)
-        component_layout.addWidget(QtWidgets.QLabel('Component:'))
+        component_layout.addWidget(QtWidgets.QLabel("Component:"))
         component_layout.addWidget(self.component_spinbox)
 
         # topomap canvas
-        self.topomaps = [TopographicMapCanvas(width=5, height=4, dpi=100) for _k in range(len(names))]
+        self.topomaps = [
+            TopographicMapCanvas(width=5, height=4, dpi=100)
+            for _k in range(len(names))
+        ]
         for topomap in self.topomaps:
             topo_layout.addWidget(topomap)
             topomap.setHidden(True)
@@ -83,17 +84,22 @@ class TopomapSelector(QtWidgets.QWidget):
     def draw_topomap(self):
         index = self.selector.current_index()
         if not self.topomap_drawn[self.current_topomap]:
-            self.topomap.update_figure(self.topographies[index][:, self.current_topomap], self.pos, names=self.names)
+            self.topomap.update_figure(
+                self.topographies[index][:, self.current_topomap],
+                self.pos,
+                names=self.names,
+            )
             self.topomap_drawn[self.current_topomap] = True
 
     def get_current_topo(self):
-        return self.topographies[self.selector.current_index()][:, self.current_topomap]
+        return self.topographies[self.selector.current_index()][
+            :, self.current_topomap
+        ]
 
     def get_current_filter(self, reject=False):
         filters = self.filters[self.selector.current_index()]
         filter = filters[:, self.current_topomap]
         if reject:
-            # rejected_matrix = dot(filters, eye(filters.shape[0]) - dot(filter[:, None], filter[None, :]))
             # inv = pinv(filters)
             # return dot(rejected_matrix, inv)
             inv = pinv(filters)
@@ -108,22 +114,27 @@ class TopomapSelector(QtWidgets.QWidget):
     def get_current_bandpass(self):
         x1 = self.selector.current_x()
         x2 = x1 + self.x_delta
-        return x1 - self.flanker_margin - self.flanker_delta, x2 + self.flanker_margin + self.flanker_delta
+        return (
+            x1 - self.flanker_margin - self.flanker_delta,
+            x2 + self.flanker_margin + self.flanker_delta,
+        )
 
     def recompute(self):
         self.topomap_drawn = [False for _topomap in self.topomaps]
         current_x = self.selector.current_x()
         parameters = self.sliders.getValues()
-        self.x_delta = parameters['bandwidth']
+        self.x_delta = parameters["bandwidth"]
         self.freqs = arange(self.x_left, self.x_right, self.x_delta)
-        self.flanker_delta = parameters['flanker_bandwidth']
-        self.flanker_margin = parameters['flanker_margin']
-        self.major_vals, self.topographies, self.filters = ssd_analysis(self.data,
-                                                                        sampling_frequency=self.sampling_freq,
-                                                                        freqs=self.freqs,
-                                                                        regularization_coef=parameters['regularizator'],
-                                                                        flanker_delta=self.flanker_delta,
-                                                                        flanker_margin=self.flanker_margin)
+        self.flanker_delta = parameters["flanker_bandwidth"]
+        self.flanker_margin = parameters["flanker_margin"]
+        self.major_vals, self.topographies, self.filters = ssd_analysis(
+            self.data,
+            sampling_frequency=self.sampling_freq,
+            freqs=self.freqs,
+            regularization_coef=parameters["regularizator"],
+            flanker_delta=self.flanker_delta,
+            flanker_margin=self.flanker_margin,
+        )
         self.selector.plot(self.freqs, self.major_vals[:, 0])
         self.selector.set_current_by_value(current_x)
         self.change_topomap()
@@ -132,27 +143,60 @@ class TopomapSelector(QtWidgets.QWidget):
         self.selector.clear_underlines_and_ticks()
         x1 = self.selector.current_x()
         x2 = x1 + self.x_delta
-        self.selector.underline(x1 - self.flanker_margin - self.flanker_delta, x1 - self.flanker_margin, 'flanker')
-        self.selector.underline(x2 + self.flanker_margin, x2 + self.flanker_margin + self.flanker_delta, 'flanker')
-        self.selector.underline(x1, x2, 'central')
+        self.selector.underline(
+            x1 - self.flanker_margin - self.flanker_delta,
+            x1 - self.flanker_margin,
+            "flanker",
+        )
+        self.selector.underline(
+            x2 + self.flanker_margin,
+            x2 + self.flanker_margin + self.flanker_delta,
+            "flanker",
+        )
+        self.selector.underline(x1, x2, "central")
         self.selector.add_xtick(x1 - self.flanker_margin - self.flanker_delta)
         self.selector.add_xtick(x2 + self.flanker_margin + self.flanker_delta)
 
     def draw_lambda_level(self):
-        self.selector.update_bin_level(self.x_delta,
-                                       self.major_vals[self.selector.current_index(), self.current_topomap])
+        self.selector.update_bin_level(
+            self.x_delta,
+            self.major_vals[
+                self.selector.current_index(), self.current_topomap
+            ],
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     import numpy as np
     from ..widgets.helpers import ch_names_to_2d_pos
-    ch_names = ['Fc1', 'Fc3', 'Fc5', 'C1', 'C3', 'C5', 'Cp1', 'Cp3', 'Cp5', 'Cz', 'Pz',
-                'Cp2', 'Cp4', 'Cp6', 'C2', 'C4', 'C6', 'Fc2', 'Fc4', 'Fc6']
+
+    ch_names = [
+        "Fc1",
+        "Fc3",
+        "Fc5",
+        "C1",
+        "C3",
+        "C5",
+        "Cp1",
+        "Cp3",
+        "Cp5",
+        "Cz",
+        "Pz",
+        "Cp2",
+        "Cp4",
+        "Cp6",
+        "C2",
+        "C4",
+        "C6",
+        "Fc2",
+        "Fc4",
+        "Fc6",
+    ]
     channels_names = np.array(ch_names)
-    x = np.loadtxt('example_recordings.txt')[:, channels_names!='Cz']
-    channels_names = list(channels_names[channels_names!='Cz'])
+    x = np.loadtxt("example_recordings.txt")[:, channels_names != "Cz"]
+    channels_names = list(channels_names[channels_names != "Cz"])
     # x = np.random.randn(10000, len(channels_names))
 
     print(x.shape, channels_names)
--- pynfb/protocols/ssd/topomap_selector_ica.py
+++ pynfb/protocols/ssd/topomap_selector_ica.py
@@ -1,23 +1,25 @@
 import h5py
-from PyQt5 import QtCore, QtGui, QtWidgets
-from mne import create_info
-from mne.io import RawArray
-from mne.preprocessing import ICA
+from PyQt5 import QtCore, QtWidgets
 from sklearn.metrics import mutual_info_score
 
 from ...io.xml_ import get_lsl_info_from_xml
 from ...postprocessing.helpers import dc_blocker
-from ...protocols.signals_manager.scored_components_table import ScoredComponentsTable
+from ...protocols.signals_manager.scored_components_table import (
+    ScoredComponentsTable,
+)
 import numpy as np
 from ...protocols.ssd.sliders_csp import Sliders
 from ...signal_processing.filters import SpatialRejection
-from ...widgets.helpers import ch_names_to_2d_pos, WaitMessage
+from ...widgets.helpers import WaitMessage
 from ..._titles import WAIT_BAR_MESSAGES
-from ...signal_processing.decompositions import CSPDecomposition, ICADecomposition, CSPDecompositionStimulus
-from ...signal_processing.helpers import stimulus_split
-from time import time
+from ...signal_processing.decompositions import (
+    CSPDecomposition,
+    ICADecomposition,
+    CSPDecompositionStimulus,
+)
 
 from ...widgets.helpers import project_to_2d
+
 
 def mutual_info(x, y, bins=100):
     c_xy = np.histogram2d(x, y, bins)[0]
@@ -26,26 +28,44 @@ def mutual_info(x, y, bins=100):
 
 
 class ICADialog(QtWidgets.QDialog):
-    def __init__(self, raw_data, channel_names, channel_pos, fs, parent=None, decomposition=None, mode='ica', filters=None,
-                 scores=None, states=None, labels=None, _stimulus_split=False, marks=None):
+    def __init__(
+        self,
+        raw_data,
+        channel_names,
+        channel_pos,
+        fs,
+        parent=None,
+        decomposition=None,
+        mode="ica",
+        filters=None,
+        scores=None,
+        states=None,
+        labels=None,
+        _stimulus_split=False,
+        marks=None,
+        band=None,
+    ):
         super(ICADialog, self).__init__(parent)
         self.setWindowTitle(mode.upper())
         self.setMinimumWidth(1200)
         self.setMinimumHeight(800)
 
         if decomposition is None:
-            if mode == 'csp':
+            if mode == "csp":
                 if not _stimulus_split:
                     self.decomposition = CSPDecomposition(channel_pos, fs)
                     if labels is None:
                         labels = np.zeros(raw_data.shape[0])
-                        labels[len(labels)//2:] = 1
+                        labels[len(labels) // 2 :] = 1
                 else:
-                    self.decomposition = CSPDecompositionStimulus(channel_pos, fs)
+                    self.decomposition = CSPDecompositionStimulus(
+                        channel_pos, fs
+                    )
                     labels = marks
-            elif mode == 'ica':
-                self.decomposition = ICADecomposition(channel_names,
-                                                      channel_pos, fs)
+            elif mode == "ica":
+                self.decomposition = ICADecomposition(
+                    channel_names, channel_pos, fs, band
+                )
         else:
             self.decomposition = decomposition
 
@@ -62,7 +82,6 @@ class ICADialog(QtWidgets.QDialog):
         self.data = self.raw_data
 
         # unmixing matrix estimation
-        timer = time()
         if decomposition is None:
             self.decomposition.fit(self.raw_data, self.labels)
         self.scores = self.decomposition.scores
@@ -70,22 +89,25 @@ class ICADialog(QtWidgets.QDialog):
         self.topographies = self.decomposition.topographies
         self.components = np.dot(self.raw_data, self.unmixing_matrix)
 
-
-        print('ICA/CSP time elapsed = {}s'.format(time() - timer))
-        timer = time()
-
-
-        scores_name = 'Mutual info' if mode == 'ica' else 'Eigenvalues'
+        scores_name = "Mutual info" if mode == "ica" else "Eigenvalues"
         # table
         pos = project_to_2d(channel_pos)
-        self.table = ScoredComponentsTable(self.components, self.topographies, self.unmixing_matrix, channel_names, fs,
-                                           self.scores, pos, scores_name=scores_name, marks=marks if _stimulus_split else None)
-        print('Table drawing time elapsed = {}s'.format(time() - timer))
+        self.table = ScoredComponentsTable(
+            self.components,
+            self.topographies,
+            self.unmixing_matrix,
+            channel_names,
+            fs,
+            self.scores,
+            pos,
+            scores_name=scores_name,
+            marks=marks if _stimulus_split else None,
+        )
 
         # reject selected button
-        self.reject_button = QtWidgets.QPushButton('Reject selection')
-        self.spatial_button = QtWidgets.QPushButton('Make spatial filter')
-        self.add_to_all_checkbox = QtWidgets.QCheckBox('Add to all signals')
+        self.reject_button = QtWidgets.QPushButton("Reject selection")
+        self.spatial_button = QtWidgets.QPushButton("Make spatial filter")
+        self.add_to_all_checkbox = QtWidgets.QCheckBox("Add to all signals")
         self.reject_button.setMaximumWidth(250)
         self.spatial_button.setMaximumWidth(250)
         self.reject_button.clicked.connect(self.reject_and_close)
@@ -94,24 +116,28 @@ class ICADialog(QtWidgets.QDialog):
         # layout
         layout = QtWidgets.QVBoxLayout(self)
         layout.addWidget(self.table)
-        self.update_band_checkbox = QtWidgets.QCheckBox('Update band')
+        self.update_band_checkbox = QtWidgets.QCheckBox("Update band")
 
         # setup sliders
-        self.sliders = Sliders(fs, reg_coef=(mode == 'csp'), stimulus_split=_stimulus_split)
+        self.sliders = Sliders(
+            fs, reg_coef=(mode == "csp"), stimulus_split=_stimulus_split
+        )
         self.sliders.apply_button.clicked.connect(self.recompute)
         self.lambda_csp3 = states
         layout.addWidget(self.sliders)
         layout.addWidget(self.update_band_checkbox)
 
         # ica mutual sorting
-        if mode == 'ica':
+        if mode == "ica":
             sort_layout = QtWidgets.QHBoxLayout()
             self.sort_combo = QtWidgets.QComboBox()
             self.sort_combo.setMaximumWidth(100)
             self.sort_combo.addItems(channel_names)
-            self.sort_combo.setCurrentIndex(self.decomposition.sorted_channel_index)
+            self.sort_combo.setCurrentIndex(
+                self.decomposition.sorted_channel_index
+            )
             self.sort_combo.currentIndexChanged.connect(self.sort_by_mutual)
-            sort_layout.addWidget(QtWidgets.QLabel('Sort by: '))
+            sort_layout.addWidget(QtWidgets.QLabel("Sort by: "))
             sort_layout.addWidget(self.sort_combo)
             sort_layout.setAlignment(QtCore.Qt.AlignLeft)
             layout.addLayout(sort_layout)
@@ -125,20 +151,37 @@ class ICADialog(QtWidgets.QDialog):
         layout.addLayout(buttons_layout)
 
         # enable maximize btn
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowMaximizeButtonHint)
+        self.setWindowFlags(
+            self.windowFlags() | QtCore.Qt.WindowMaximizeButtonHint
+        )
 
         # checkboxes behavior
-        self.table.no_one_selected.connect(lambda: self.reject_button.setDisabled(True))
-        self.table.no_one_selected.connect(lambda: self.spatial_button.setDisabled(True))
-        self.table.one_selected.connect(lambda: self.reject_button.setDisabled(False))
-        self.table.one_selected.connect(lambda: self.spatial_button.setDisabled(False))
-        self.table.more_one_selected.connect(lambda: self.reject_button.setDisabled(False))
-        self.table.more_one_selected.connect(lambda: self.spatial_button.setDisabled(True))
+        self.table.no_one_selected.connect(
+            lambda: self.reject_button.setDisabled(True)
+        )
+        self.table.no_one_selected.connect(
+            lambda: self.spatial_button.setDisabled(True)
+        )
+        self.table.one_selected.connect(
+            lambda: self.reject_button.setDisabled(False)
+        )
+        self.table.one_selected.connect(
+            lambda: self.spatial_button.setDisabled(False)
+        )
+        self.table.more_one_selected.connect(
+            lambda: self.reject_button.setDisabled(False)
+        )
+        self.table.more_one_selected.connect(
+            lambda: self.spatial_button.setDisabled(True)
+        )
         self.table.checkboxes_state_changed()
 
     def sort_by_mutual(self):
         ind = self.sort_combo.currentIndex()
-        self.scores = [mutual_info(self.components[:, j], self.data[:, ind]) for j in range(self.components.shape[1])]
+        self.scores = [
+            mutual_info(self.components[:, j], self.data[:, ind])
+            for j in range(self.components.shape[1])
+        ]
         self.table.set_scores(self.scores)
 
     def reject_and_close(self):
@@ -146,54 +189,132 @@ class ICADialog(QtWidgets.QDialog):
         unmixing_matrix = self.unmixing_matrix.copy()
         inv = np.linalg.pinv(self.unmixing_matrix)
         unmixing_matrix[:, indexes] = 0
-        self.rejection = SpatialRejection(np.dot(unmixing_matrix, inv), rank=len(indexes), type_str=self.mode,
-                                          topographies=self.topographies[:, indexes])
-        self.close()
+        self.rejection = SpatialRejection(
+            np.dot(unmixing_matrix, inv),
+            rank=len(indexes),
+            type_str=self.mode,
+            topographies=self.topographies[:, indexes],
+        )
+        self.accept()
 
     def spatial_and_close(self):
         index = self.table.get_checked_rows()[0]
-        self.spatial =  self.unmixing_matrix[:, index]
+        self.spatial = self.unmixing_matrix[:, index]
         self.topography = self.topographies[:, index]
-        print(index)
         self.close()
 
     def recompute(self):
         parameters = self.sliders.getValues()
-        self.bandpass = (parameters['bandpass_low'], parameters['bandpass_high'])
+        self.bandpass = (
+            parameters["bandpass_low"],
+            parameters["bandpass_high"],
+        )
         self.decomposition.set_parameters(**parameters)
         self.decomposition.fit(self.raw_data, self.labels)
         self.scores = self.decomposition.scores
         self.unmixing_matrix = self.decomposition.filters
         self.topographies = self.decomposition.topographies
         self.components = np.dot(self.raw_data, self.unmixing_matrix)
-        self.table.redraw(self.components, self.topographies, self.unmixing_matrix, self.scores)
+        self.table.redraw(
+            self.components,
+            self.topographies,
+            self.unmixing_matrix,
+            self.scores,
+        )
 
     @classmethod
-    def get_rejection(cls, raw_data, channel_names, fs, decomposition=None, mode='ica', states=None, labels=None, _stimulus_split=False, marks=None):
-        wait_bar = WaitMessage(mode.upper() + WAIT_BAR_MESSAGES['CSP_ICA']).show_and_return()
-        selector = cls(raw_data, channel_names, fs, decomposition=decomposition, mode=mode, states=states, labels=labels, _stimulus_split=_stimulus_split, marks=marks)
+    def get_rejection(
+        cls,
+        raw_data,
+        channel_names,
+        fs,
+        decomposition=None,
+        mode="ica",
+        states=None,
+        labels=None,
+        _stimulus_split=False,
+        marks=None,
+    ):
+        wait_bar = WaitMessage(
+            mode.upper() + WAIT_BAR_MESSAGES["CSP_ICA"]
+        ).show_and_return()
+        selector = cls(
+            raw_data,
+            channel_names,
+            fs,
+            decomposition=decomposition,
+            mode=mode,
+            states=states,
+            labels=labels,
+            _stimulus_split=_stimulus_split,
+            marks=marks,
+        )
         wait_bar.close()
         result = selector.exec_()
-        bandpass = selector.bandpass if selector.update_band_checkbox.isChecked() else None
-        return (selector.rejection,
-                selector.spatial, selector.topography,
-                selector.decomposition,
-                bandpass,
-                selector.add_to_all_checkbox.isChecked())
+        bandpass = (
+            selector.bandpass
+            if selector.update_band_checkbox.isChecked()
+            else None
+        )
+        return (
+            selector.rejection,
+            selector.spatial,
+            selector.topography,
+            selector.decomposition,
+            bandpass,
+            selector.add_to_all_checkbox.isChecked(),
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import numpy as np
 
     app = QtWidgets.QApplication([])
     n_channels = 3
     fs = 100
 
-    channels = ['Cp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'Ft9', 'Fc5', 'Fc1', 'Fc2', 'Fc6', 'Ft10', 'T7', 'C3', 'Cz',
-                'C4', 'T8', 'Tp9', 'Cp5', 'Cp1', 'Cp2', 'Cp6', 'Tp10', 'P7', 'P3', 'Pz', 'P4', 'P8', 'O1', 'Oz', 'O2']
+    channels = [
+        "Cp1",
+        "Fp2",
+        "F7",
+        "F3",
+        "Fz",
+        "F4",
+        "F8",
+        "Ft9",
+        "Fc5",
+        "Fc1",
+        "Fc2",
+        "Fc6",
+        "Ft10",
+        "T7",
+        "C3",
+        "Cz",
+        "C4",
+        "T8",
+        "Tp9",
+        "Cp5",
+        "Cp1",
+        "Cp2",
+        "Cp6",
+        "Tp10",
+        "P7",
+        "P3",
+        "Pz",
+        "P4",
+        "P8",
+        "O1",
+        "Oz",
+        "O2",
+    ]
     channels = channels[:n_channels]
 
-    x = np.array([np.sin(10 * (f + 1) * 2 * np.pi * np.arange(0, 10, 1 / fs)) for f in range(n_channels)]).T
+    x = np.array(
+        [
+            np.sin(10 * (f + 1) * 2 * np.pi * np.arange(0, 10, 1 / fs))
+            for f in range(n_channels)
+        ]
+    ).T
 
     # Generate sample data
     np.random.seed(0)
@@ -214,29 +335,35 @@ if __name__ == '__main__':
     A = np.array([[1, 1, 1], [0.5, 2, 1.0], [1.5, 1.0, 2.0]])  # Mixing matrix
     x = np.dot(S, A.T)  # Generate observations
     y = np.ones(len(x))
-    y[:len(y)//2] = 0
-
+    y[: len(y) // 2] = 0
 
     if False:
-        dir_ = 'D:\\vnd_spbu\\pilot\\mu5days'
-        experiment = 'pilot5days_Skotnikova_Day4_03-02_13-33-55'
-        with h5py.File('{}\\{}\\{}'.format(dir_, experiment, 'experiment_data.h5')) as f:
-            ica = f['protocol1/signals_stats/left/rejections/rejection1'][:]
+        dir_ = "D:\\vnd_spbu\\pilot\\mu5days"
+        experiment = "pilot5days_Skotnikova_Day4_03-02_13-33-55"
+        with h5py.File(
+            "{}\\{}\\{}".format(dir_, experiment, "experiment_data.h5")
+        ) as f:
+            ica = f["protocol1/signals_stats/left/rejections/rejection1"][:]
 
-            x_filters = dc_blocker(np.dot(f['protocol1/raw_data'][:], ica))
-            x_rotation = dc_blocker(np.dot(f['protocol2/raw_data'][:], ica))
+            x_filters = dc_blocker(np.dot(f["protocol1/raw_data"][:], ica))
+            x_rotation = dc_blocker(np.dot(f["protocol2/raw_data"][:], ica))
             x_dict = {
-                'closed': x_filters[:x_filters.shape[0] // 2],
-                'opened': x_filters[x_filters.shape[0] // 2:],
-                'rotate': x_rotation
+                "closed": x_filters[: x_filters.shape[0] // 2],
+                "opened": x_filters[x_filters.shape[0] // 2 :],
+                "rotate": x_rotation,
             }
-            x = np.concatenate([x_dict['closed'], x_dict['opened'], x_dict['rotate']])
-            drop_channels = ['AUX', 'A1', 'A2']
-            labels, fs = get_lsl_info_from_xml(f['stream_info.xml'][0])
-            print('fs: {}\nall labels {}: {}'.format(fs, len(labels), labels))
-            channels = [label for label in labels if label not in drop_channels]
+            x = np.concatenate(
+                [x_dict["closed"], x_dict["opened"], x_dict["rotate"]]
+            )
+            drop_channels = ["AUX", "A1", "A2"]
+            labels, fs = get_lsl_info_from_xml(f["stream_info.xml"][0])
+            channels = [
+                label for label in labels if label not in drop_channels
+            ]
 
     for j in range(4):
-        rejection, spatial, unmixing = ICADialog.get_rejection(x, channels, fs, mode='ica', labels=y)
+        rejection, spatial, unmixing = ICADialog.get_rejection(
+            x, channels, fs, mode="ica", labels=y
+        )
         if rejection is not None:
             x = np.dot(x, rejection)
